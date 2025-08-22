7c2a7226d8a85d7f614947dd855aeba85e197163
zhepingjiang <zheping.jiang@gmail.com>
2025-08-22 00:35:00 -0400

    Initial Commit

---
+++ src/main/java/com/laioffer/twitch/TwitchApplication.java
@@ -0,0 +1,13 @@
+package com.laioffer.twitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchApplication.class, args);
+	}
+
+}
