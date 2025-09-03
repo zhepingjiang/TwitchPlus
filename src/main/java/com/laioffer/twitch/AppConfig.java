a73bbc1f0d390d6407d20c2fc6f91eb848fbed8d
zhepingjiang <zheping.jiang@gmail.com>
2025-09-03 00:27:51 -0400

    12. Declarative HTTP Client: 1. Add Twitch POJOs, 2. Update gradle dependencies for openFeign, 3. App settings, 4. Test runner to test out Twitch APIs

---
+++ src/main/java/com/laioffer/twitch/AppConfig.java
@@ -0,0 +1,21 @@
+package com.laioffer.twitch;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/**").permitAll()
+                );
+        return http.build();
+    }
+}
